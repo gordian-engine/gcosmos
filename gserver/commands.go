2ad0198228394f29531a24e1faf5d566b70cceca
Andrew Gouin <andrew@gouin.io>
2024-10-28 20:43:21 -0600

    handle ctx done

--- gserver/commands.go
+++ gserver/commands.go
@@ -76,7 +76,11 @@ func newSeedCommand() *cobra.Command {
 
 			c := make(chan os.Signal, 1)
 			signal.Notify(c, os.Interrupt)
-			<-c
+
+			select {
+			case <-ctx.Done():
+			case <-c:
+			}
 
 			return nil
 		},
